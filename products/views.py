7cafabf7f7ee18b55150b7f4df2acebff5fab478
maish79 <maishsupa@yahoo.com>
2023-06-14 11:56:22 +0000

    added sorting category functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 
-from .models import Product
+from .models import Product, Category
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
@@ -12,8 +12,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split('_')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -26,6 +32,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
