ca502b0ac3bfa3677e74ab59acbd1ca8c94a981e
maish79 <maishsupa@yahoo.com>
2023-06-14 11:37:14 +0000

    added search fuctionality to filter the words by name

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+
 from .models import Product
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 
@@ -9,9 +11,21 @@ def all_products(request):
     including sorting and searching queries"""  
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
