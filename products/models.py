7588a3193947790366b27751e4779e752bc36158
maish79 <maishsupa@yahoo.com>
2023-06-15 11:41:59 +0000

    update db and product detail html

--- products/models.py
+++ products/models.py
@@ -23,6 +23,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    has_other_flavors = models.BooleanField(default=False, null=True, blank=True)
+    customized_message = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
