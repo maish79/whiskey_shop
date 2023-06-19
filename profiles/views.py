ea1dcdccb9745c633263f9ad4dd957128b46053e
maish79 <maishsupa@yahoo.com>
2023-06-19 15:15:10 +0000

    add profile app, template and urls

---
+++ profiles/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+
+
+# Create your views here.
+def profile(request):
+    """ Display profile """
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
