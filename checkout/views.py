0c12a3548705a130849f8a5d601c2bdc0d824311
maish79 <maishsupa@yahoo.com>
2023-06-20 17:53:38 +0000

    added order history to profile and edit profile models

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from bag.contexts import bag_contents
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 import stripe
 import json
 
@@ -99,8 +101,24 @@ def checkout(request):
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
     )
-
-    order_form = OrderForm()
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            order_form = OrderForm(initial={
+                'full_name': profile.user.get_full_name(),
+                'email': profile.user.email,
+                'phone_number': profile.default_phone_number,
+                'country': profile.default_country,
+                'postcode': profile.default_postcode,
+                'city': profile.default_city,
+                'street_address1': profile.default_street_address1,
+                'street_address2': profile.default_street_address2,
+                'county': profile.default_county,
+            })
+        except UserProfile.DoesNotExist:
+            order_form = OrderForm()
+    else:
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -123,6 +141,29 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_name = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_county': order.county,
+                'default_city': order.city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_postcode': order.postcode,
+                'default_country': order.country,
+            }
+            user_name_form= UserProfileForm(profile_data,instance=profile)
+            if user_name_form.is_valid():
+                user_name_form.save()
+
+
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
--- profiles/models.py
+++ profiles/models.py
@@ -12,10 +12,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True,
                                             blank=True)
-    default_country = CountryField(blank_label='Country *', null=True,
-                                   blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True,
                                                blank=True)
@@ -24,8 +21,6 @@ class UserProfile(models.Model):
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country', null=True,
                                    blank=True)
-
-
     def __str__(self):
         return self.user.username
 
@@ -37,5 +32,8 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.userprofile.save()
+    else:
+        try:
+            instance.userprofile.save()
+        except UserProfile.DoesNotExist:
+            UserProfile.objects.create(user=instance)
