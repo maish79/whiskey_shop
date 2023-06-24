0b1d6fb0303df3c07a3eabe379e37a33d91ab785
maish79 <maishsupa@yahoo.com>
2023-06-24 13:43:03 +0000

    edit product model and template

--- products/models.py
+++ products/models.py
@@ -21,10 +21,10 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    main_flavor = models.CharField(max_length=40, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     has_other_flavors = models.BooleanField(default=False, null=True, blank=True)
-    customized_message = models.BooleanField(default=False, null=True, blank=True)
+    
 
     def __str__(self):
         return self.name
--- whiskey_shop/settings.py
+++ whiskey_shop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get('SECRET_KEY', '')
+SECRET_KEY =  'matako'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = [ '8000-maish79-whiskeyshop-4iazfi7wiif.ws-eu100.gitpod.io', 'whiskey-shop-320e32983cd1.herokuapp.com' , 'localhost' ]
