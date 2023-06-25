7fbf6f44fbd62842b483c42f52f9cc117556edc7
maish79 <maishsupa@yahoo.com>
2023-06-25 13:49:17 +0000

    added about-us template, views and urls

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
-
-from django.contrib import admin
 from django.urls import path 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('FAQ/', views.FAQ, name='FAQ'),
     
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,12 @@ def index(request):
     """A view to return the index page"""
 
     return render(request, 'home/index.html')
+
+def about_us(request):
+    """A view to return the about us page"""
+    return render(request, 'home/about_us.html')
+
+
+def FAQ(request):
+    """A view to return the FAQ us page"""
+    return render(request, 'home/FAQ.html')
