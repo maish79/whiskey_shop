cb0f48c755fdee4a5e95b58ae9a9c375bdba9dec
maish79 <maishsupa@yahoo.com>
2023-06-29 11:54:48 +0000

    added location unit test

--- location/tests.py
+++ location/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestLocationViews(TestCase):
+    """
+    A class for testing location views
+    """
+    def test_get_location_page(self):
+        """
+        This test checks if the location page is displayed
+        """
+        response = self.client.get('/locations/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'locations.html')
+
+    def test_get_add_location_page(self):
+        """
+        This test checks if the add location page is displayed
+        """
+        response = self.client.get('/locations/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_location.html')
