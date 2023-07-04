2182d74c92adbbb66467a0544463191955fa89cc
maish79 <maishsupa@yahoo.com>
2023-07-04 19:06:51 +0000

    added static root variable

--- whiskey_shop/settings.py
+++ whiskey_shop/settings.py
@@ -28,7 +28,8 @@ ALLAUTH_TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates', 'allauth')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '&d6qk%@=um#dvjqwm29r68%lgu#6_t!9y_*=$30w)uy)#z%gp*'
+              
               
               
 
@@ -192,6 +193,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
