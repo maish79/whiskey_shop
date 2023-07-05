033096dfbd53b2e1768d239767e2a01498b6cf29
maish79 <maishsupa@yahoo.com>
2023-07-05 13:24:21 +0000

    deployment commit

--- whiskey_shop/settings.py
+++ whiskey_shop/settings.py
@@ -29,7 +29,7 @@ ALLAUTH_TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates', 'allauth')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'matako'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
               
               
               
