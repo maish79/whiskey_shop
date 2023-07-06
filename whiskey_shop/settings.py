622dadf4e9782876fc96530a9c4d46709a084ea6
maish79 <maishsupa@yahoo.com>
2023-07-06 17:57:44 +0000

    edit settings

--- whiskey_shop/settings.py
+++ whiskey_shop/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'matako'
               
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = [  '8000-maish79-whiskeyshop-1qhicexe84q.ws-eu101.gitpod.io', 'whiskey-shop-320e32983cd1.herokuapp.com' , 'localhost' ]
@@ -110,13 +110,13 @@ TEMPLATES = [
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 
-AUTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
     
-)
+]
 
 SITE_ID = 1
 
