5682cbb6b88c18384d5706031341664e5a161f0e
maish79 <maishsupa@yahoo.com>
2023-07-06 18:16:52 +0000

    edit settings.py

--- whiskey_shop/settings.py
+++ whiskey_shop/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = 'DEVELOPMENT' in os.environ
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
               
               
 
